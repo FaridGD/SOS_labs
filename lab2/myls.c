147254f9f3979bbe7b06c4eb7b57562f4c5a5767
Farid <aliev.mr-farid@yandex.ru>
2025-10-07 14:05:16 +0300

    Myls

---
+++ lab2/myls.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <dirent.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
+
+typedef struct
+{
+	bool a;
+	bool l;
+} Fl;
+
+void print_permissions(mode_t mode) {
+    printf((S_ISDIR(mode)) ? "d" : "-");
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
+}
+
+void print_file_info(const char *filename) {
+    struct stat file_stat;
+
+    if (stat(filename, &file_stat) == -1) {
+        perror("stat");
+        return;
+    }
+
+    print_permissions(file_stat.st_mode);
+
+    printf(" %lu", file_stat.st_nlink);
+
+    struct passwd *pw = getpwuid(file_stat.st_uid);
+    struct group *gr = getgrgid(file_stat.st_gid);
+    printf(" %s %s", pw->pw_name, gr->gr_name);
+
+    printf(" %ld", file_stat.st_size);
+
+    char time_buf[20];
+    struct tm *timeinfo = localtime(&file_stat.st_mtime);
+    strftime(time_buf, sizeof(time_buf), "%b %d %H:%M", timeinfo);
+    printf(" %s", time_buf);
+    printf(" %s\n", filename);
+}
+
+bool is_directory(const char* path)
+{
+	struct stat st;
+	if (stat(path, &st) != 0)
+	{
+		return false;
+	}
+	return S_ISDIR(st.st_mode);
+}
+
+void hpath(const char* path, Fl flags)
+{
+	long total_blocks = 0;
+	if (!is_directory(path))
+	{
+		fprintf(stderr, "Ошибка: директория не найдена\n");
+		return;
+	}
+	
+	DIR *dir = opendir(path);
+	if (!dir)
+	{
+		fprintf(stderr, "Не удалось открыть директоиию\n");
+		return;
+	}
+
+	struct dirent* entry;
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (!flags.a && entry->d_name[0] == '.')
+			continue;
+
+		char full_path[1024];
+
+		snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+		struct stat file_stat;
+
+		if (lstat(full_path, &file_stat) == 0)
+		{
+			total_blocks += file_stat.st_blocks;
+		}
+	}
+	if (flags.l && total_blocks > 0)
+	{
+		printf("total %ld\n", total_blocks / 2);
+	}
+	rewinddir(dir);
+	while ((entry = readdir(dir)) != NULL)
+	{	
+		if (!flags.a && entry->d_name[0] == '.')
+		{
+			continue;
+		}
+		if (flags.l)
+		{
+			print_file_info(entry->d_name);
+		}
+		else
+		{
+			printf("%s\n", entry->d_name);
+		}
+	}
+	closedir(dir);
+}
+
+
+int main(int argc, char* argv[])
+{
+	Fl flags = {false, false};
+
+	int opt;
+	while ((opt = getopt(argc, argv, "al")) != -1)
+	{
+		switch (opt)
+		{
+			case 'a':
+				flags.a = true;
+				break;
+			case 'l':
+				flags.l = true;
+				break;
+		}
+
+	}
+	if (optind == argc)
+	{
+        	hpath(".", flags);
+	}
+	else
+	{
+        	for (int i = optind; i < argc; i++)
+		{
+			hpath(argv[i], flags);
+		}
+        }
+
+    return 0;
+}
