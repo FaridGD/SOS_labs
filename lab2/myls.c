cf5f1c71ac7c8c462b8bf96e107b4a3ff7aa114a
Farid <aliev.mr-farid@yandex.ru>
2025-10-09 15:59:54 +0300

    Fixes

--- lab2/myls.c
+++ lab2/myls.c
@@ -19,23 +19,23 @@
 
 int compare_names(const void *a, const void *b)
 {
-    const char *nameA = *(const char **)a;
-    const char *nameB = *(const char **)b;
-    return strcasecmp(nameA, nameB);
+	const char *nameA = *(const char **)a;
+	const char *nameB = *(const char **)b;
+	return strcasecmp(nameA, nameB);
 }
 
 const char* get_file_color(mode_t mode)
 {
-    if (S_ISDIR(mode)) return COLOR_BLUE;
-    if (S_ISLNK(mode)) return COLOR_CYAN;
-    if (mode & S_IXUSR) return COLOR_GREEN;
-    return COLOR_RESET;
+	if (S_ISDIR(mode)) return COLOR_BLUE;
+	if (S_ISLNK(mode)) return COLOR_CYAN;
+	if (mode & S_IXUSR) return COLOR_GREEN;
+	return COLOR_RESET;
 }
 
 void print_colored_name(const char* name, mode_t mode)
 {
-    const char* color = get_file_color(mode);
-    printf("%s%s%s", color, name, COLOR_RESET);
+	const char* color = get_file_color(mode);
+	printf("%s%s%s", color, name, COLOR_RESET);
 }
 
 typedef struct
@@ -46,45 +46,39 @@ typedef struct
 
 void print_permissions(mode_t mode)
 {
-    printf((S_ISDIR(mode)) ? "d" : "-");
-    printf((mode & S_IRUSR) ? "r" : "-");
-    printf((mode & S_IWUSR) ? "w" : "-");
-    printf((mode & S_IXUSR) ? "x" : "-");
-    printf((mode & S_IRGRP) ? "r" : "-");
-    printf((mode & S_IWGRP) ? "w" : "-");
-    printf((mode & S_IXGRP) ? "x" : "-");
-    printf((mode & S_IROTH) ? "r" : "-");
-    printf((mode & S_IWOTH) ? "w" : "-");
-    printf((mode & S_IXOTH) ? "x" : "-");
+	printf((S_ISDIR(mode)) ? "d" : "-");
+	printf((mode & S_IRUSR) ? "r" : "-");
+	printf((mode & S_IWUSR) ? "w" : "-");
+	printf((mode & S_IXUSR) ? "x" : "-");
+	printf((mode & S_IRGRP) ? "r" : "-");
+	printf((mode & S_IWGRP) ? "w" : "-");
+	printf((mode & S_IXGRP) ? "x" : "-");
+	printf((mode & S_IROTH) ? "r" : "-");
+	printf((mode & S_IWOTH) ? "w" : "-");
+	printf((mode & S_IXOTH) ? "x" : "-");
 }
 
 void print_file_info(const char *filename, const char *display_name)
 {
-    struct stat file_stat;
-
-    if (stat(filename, &file_stat) != 0)
-    {
-        perror("lstat");
-        return;
-    }
-
-    print_permissions(file_stat.st_mode);
-
-    printf(" %lu", file_stat.st_nlink);
-
-    struct passwd *pw = getpwuid(file_stat.st_uid);
-    struct group *gr = getgrgid(file_stat.st_gid);
-    printf(" %s %s", pw ? pw->pw_name : "?", gr ? gr->gr_name : "?");
-
-    printf(" %ld", file_stat.st_size);
-
-    char time_buf[20];
-    struct tm *timeinfo = localtime(&file_stat.st_mtime);
-    strftime(time_buf, sizeof(time_buf), "%b %d %H:%M", timeinfo);
-    printf(" %s", time_buf);
-    printf(" ");
-    print_colored_name(display_name, file_stat.st_mode);
-    printf("\n");
+	struct stat file_stat;
+	if (stat(filename, &file_stat) != 0)
+	{
+		perror("lstat");
+		return;
+	}
+	print_permissions(file_stat.st_mode);
+	printf(" %lu", file_stat.st_nlink);
+	struct passwd *pw = getpwuid(file_stat.st_uid);
+	struct group *gr = getgrgid(file_stat.st_gid);
+	printf(" %s %s", pw ? pw->pw_name : "?", gr ? gr->gr_name : "?");
+	printf(" %ld", file_stat.st_size);
+	char time_buf[20];
+	struct tm *timeinfo = localtime(&file_stat.st_mtime);
+	strftime(time_buf, sizeof(time_buf), "%b %d %H:%M", timeinfo);
+	printf(" %s", time_buf);
+	printf(" ");
+	print_colored_name(display_name, file_stat.st_mode);
+	printf("\n");
 }
 
 bool is_directory(const char* path)
@@ -99,13 +93,13 @@ bool is_directory(const char* path)
 
 char* my_strdup(const char* str)
 {
-    size_t len = strlen(str) + 1;
-    char* new_str = malloc(len);
-    if (new_str)
-    {
-        memcpy(new_str, str, len);
-    }
-    return new_str;
+	size_t len = strlen(str) + 1;
+	char* new_str = malloc(len);
+	if (new_str)
+	{
+		memcpy(new_str, str, len);
+	}
+	return new_str;
 }
 
 void hpath(const char* path, Fl flags)
